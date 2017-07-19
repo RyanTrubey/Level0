3cf2e6fd5e4af61522b862f0f33abe1c13c17c9c
RyanTrubey <league@WorthyMini-5.attlocal.net>
2017-07-19 12:01:47 -0700

    ForLoopGauntlet

---
+++ Level0-Ryan Trubey/src/ForLoopGauntlet.java
@@ -0,0 +1,46 @@
+
+public class ForLoopGauntlet {
+public static void main(String[] args) {
+	
+	/* for(int i = 0; i <= 100; i++){
+		System.out.println(i);
+	} */
+	
+	/* for(int i = 100; i >= 0; i--){
+		System.out.println(i);
+	} */
+	
+	/* for(int i = 1; i <= 100; i++){
+		if(i%2 == 0){
+			System.out.println(i);
+		}
+	} */
+	
+	/* for(int i = 1; i <= 100; i++){
+		if(i%2 == 1){
+			System.out.println(i);
+		}
+	} */
+	
+	/* for(int i = 1; i <= 500; i++){
+		if(i%2 == 1){
+			System.out.println(i + " odd");
+		} else if (i%2 == 0){
+			System.out.println(i + " even");
+		}
+	} */
+	
+	/*for(int i = 0; i <= 77; i++){
+		if(i%7 == 0){
+			System.out.println(i);
+		}
+	} */
+	
+	for(int i = 0; i <= 2; i++){
+		for(int j = 0; j <= 2; j++){
+			System.out.println(i + "" + j);
+		}
+	}
+	
+}
+}
