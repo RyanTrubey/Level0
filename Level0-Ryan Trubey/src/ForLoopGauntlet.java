600847d5e204e1c0a6b348d0038a09853489bab0
RyanTrubey <league@WorthyMini-5.attlocal.net>
2017-07-20 11:54:06 -0700

    Forloopgauntlet

--- Level0-Ryan Trubey/src/ForLoopGauntlet.java
+++ Level0-Ryan Trubey/src/ForLoopGauntlet.java
@@ -36,11 +36,43 @@ public static void main(String[] args) {
 		}
 	} */
 	
-	for(int i = 0; i <= 2; i++){
+	/* for(int i = 0; i <= 2; i++){
 		for(int j = 0; j <= 2; j++){
 			System.out.println(i + "" + j);
 		}
+	} */
+	
+	/* for(int i = 1; i <= 7; i+=3){
+		for(int j = i+1; j <= 8; j+= 30){
+			for(int l = i+2; l <= 9; l+= 30){
+				System.out.println(i + " " + j + " " + l);
+			}
+		}
+	} */
+	
+	/* for(int a = 1; a <= 100; a += 10){
+		int b = a+1;
+		int c = a+2;
+		int d = a+3;
+		int e = a+4;
+		int f = a+5;
+		int g = a+6;
+		int h = a+7;
+		int i = a+8;
+		int j = a+9;
+		System.out.println(a + " " + b + " " + c + " " + d + " " + e + " " + f + " " + g + " " + " " + h + " " + i + " " + j );
+	} */
+
+	for(int i = 1; i < 7; i++){
+		
+		for(int j = 0; j <= i; j++){
+			System.out.print("*");
+		}
 	}
 	
+	/* for(int i = 0; i > -101; i-=1){
+		int j = i+100;
+		System.out.println(j);
+	} */
 }
 }
